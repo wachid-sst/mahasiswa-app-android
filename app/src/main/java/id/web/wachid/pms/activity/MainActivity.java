898b165ffdab8f199a51c329c27004146eaabb00
abdur rochman <wachid.sst@gmail.com>
2017-12-04 00:29:07 +0700

    Menambahkan bottom nafigation dan fragment

--- app/src/main/java/id/web/wachid/pms/activity/MainActivity.java
+++ app/src/main/java/id/web/wachid/pms/activity/MainActivity.java
@@ -1,13 +1,19 @@
 package id.web.wachid.pms.activity;
 
-import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import id.web.wachid.pms.fragment.AddFragment;
+import id.web.wachid.pms.fragment.FavoriteFragment;
+import id.web.wachid.pms.fragment.HomeFragment;
 import id.web.wachid.pms.R;
+import id.web.wachid.pms.fragment.ProfileFragment;
+import id.web.wachid.pms.fragment.SearchFragment;
 import id.web.wachid.pms.util.SharedPrefManager;
 
 import butterknife.BindView;
@@ -15,14 +21,21 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.tvResultNama)
-    TextView tvResultNama;
-    @BindView(R.id.btnLogout)
-    Button btnLogout;
-    @BindView(R.id.btnLihatDosen)
-    Button btnLihatDosen;
-    @BindView(R.id.btnLihatMatkul)
-    Button btnLihatMatkul;
+
+
+    @BindView(R.id.ivHome)
+    ImageView ivHome;
+    @BindView(R.id.ivSearh)
+    ImageView ivSearch;
+    @BindView(R.id.ivAdd)
+    ImageView ivAdd;
+    @BindView(R.id.ivFavorite)
+    ImageView ivFavorite;
+    @BindView(R.id.ivProfile)
+    ImageView ivProfile;
+
+    @BindView(R.id.frameFragment)
+    FrameLayout frameFragment;
 
     SharedPrefManager sharedPrefManager;
 
@@ -32,32 +45,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-        sharedPrefManager = new SharedPrefManager(this);
 
-        tvResultNama.setText(sharedPrefManager.getSPNama());
+        initFragment(new HomeFragment());
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+        ivHome.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_SUDAH_LOGIN, false);
-                startActivity(new Intent(MainActivity.this, LoginActivity.class)
-                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
-                finish();
+            public void onClick(View view) {
+                initFragment(new HomeFragment());
             }
         });
 
-        btnLihatDosen.setOnClickListener(new View.OnClickListener() {
+        ivSearch.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, DosenActivity.class));
+            public void onClick(View view) {
+                initFragment(new SearchFragment());
             }
         });
 
-        btnLihatMatkul.setOnClickListener(new View.OnClickListener() {
+        ivAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MatkulActivity.class));
+            public void onClick(View view) {
+                initFragment(new AddFragment());
             }
         });
+
+        ivFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initFragment(new FavoriteFragment());
+            }
+        });
+
+        ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initFragment(new ProfileFragment());
+            }
+        });
+    }
+
+    private void initFragment(Fragment classFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameFragment, classFragment);
+        transaction.commit();
     }
 }
